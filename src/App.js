ca219ca8231d77e90a00e3bfbc2a7de83e948a96
Bhargu2 <bhargavie.gedela@gmail.com>
2025-03-23 22:30:53 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}  />
+        <Route path="/Login" element={<Login />}  />
+        <Route path="/Register" element={<Register />}  />
+        
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1 align="center">Welcome to the Foodie App where you can Enjoy Delicious Food</h1>
+      <img src="img.jpg" alt="" />
+    </div>
+  )
+}
+
+export default Home
